ef6ebc572d25ede98e90c94064b51591fd31bbe6
Igor.E.Novikov@gmail.com <Igor.E.Novikov@gmail.com@35de2949-7618-d51d-049f-176905232806>
2011-04-02 00:49:26 +0000

    presenter api and page origin

---
+++ trunk/src/skencil/api.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2011 by Igor E. Novikov
+#	
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#	
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+from sk1app import events
+from uc2.sk1doc import model
+
+class PresenterAPI:
+
+	presenter = None
+	view = None
+	methods = None
+	model = None
+	app = None
+	eventloop = None
+	undo = []
+	redo = []
+	undo_marked = False
+	view_items = {}
+
+	def __init__(self, presenter):
+		self.presenter = presenter
+		self.methods = self.presenter.methods
+		self.model = presenter.model
+		self.view = presenter.canvas
+
+		self.eventloop = presenter.eventloop
+		self.app = presenter.app
+		self.undo = []
+		self.redo = []
+
+	def do_undo(self):
+#		canvas = self.presenter.view
+#		canvas.set_temp_cursor(canvas.wait_cursor)
+		transaction_list = self.undo[-1][0]
+		for transaction in transaction_list:
+			self._do_action(transaction)
+		tr = self.undo[-1]
+		self.undo.remove(tr)
+		self.redo.append(tr)
+		self.eventloop.emit(self.eventloop.DOC_MODIFIED)
+		if self.undo and self.undo[-1][2]:
+			self.presenter.reflect_saving()
+		if not self.undo and not self.undo_marked:
+			self.presenter.reflect_saving()
+#		if self.presenter.selection.items:
+#			self.view.sel_item.refresh()
+#		canvas.restore_cursor()
+
+	def do_redo(self):
+#		canvas = self.presenter.view
+#		canvas.set_temp_cursor(canvas.wait_cursor)
+		transaction_list = self.redo[-1][1]
+		for transaction in transaction_list:
+			self._do_action(transaction)
+		tr = self.redo[-1]
+		self.redo.remove(tr)
+		self.undo.append(tr)
+		self.eventloop.emit(self.eventloop.DOC_MODIFIED)
+		if not self.undo or self.undo[-1][2]:
+			self.presenter.reflect_saving()
+#		if self.presenter.selection.items:
+#			self.view.sel_item.refresh()
+#		canvas.restore_cursor()
+
+	def _do_action(self, action):
+		if not action: return
+		if len(action) == 1:
+			action[0]()
+		elif len(action) == 2:
+			action[0](action[1])
+		elif len(action) == 3:
+			action[0](action[1], action[2])
+		elif len(action) == 4:
+			action[0](action[1], action[2], action[3])
+		elif len(action) == 5:
+			action[0](action[1], action[2], action[3], action[4])
+		elif len(action) == 6:
+			action[0](action[1], action[2], action[3], action[4], action[5])
+
+	def add_undo(self, transaction):
+		self.redo = []
+		self.undo.append(transaction)
+		self.eventloop.emit(self.eventloop.DOC_MODIFIED)
+
+	def save_mark(self):
+		for item in self.undo:
+			item[2] = False
+		for item in self.redo:
+			item[2] = False
+
+		if self.undo:
+			self.undo[-1][2] = True
+			self.undo_marked = True
+
+	def clear_history(self):
+		self.undo = []
+		self.redo = []
+		events.emit(events.DOC_CHANGED, self.presenter)
+
+	def set_doc_origin(self, origin):
+		cur_origin = self.model.doc_origin
+		transaction = [
+			[[self.methods.set_doc_origin, cur_origin]],
+			[[self.methods.set_doc_origin, origin]],
+			False]
+		self.methods.set_doc_origin(origin)
+		self.add_undo(transaction)
+
+
+
+	def create_rectangle(self, rect):
+		rect = [rect.x(), rect.y(), rect.width(), rect.height()]
+		config = self.app.config
+		parent = self.presenter.active_layer
+		obj = model.Rectangle(config, parent, rect)
+		index = len(parent.childs)
+#		self.insert_object(obj, None, parent, index)
+
+
