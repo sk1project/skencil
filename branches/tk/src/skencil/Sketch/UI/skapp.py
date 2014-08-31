d08e21493001841201c43f9c4ef83a5a28ae2e44
igor.e.novikov@gmail.com <igor.e.novikov@gmail.com@35de2949-7618-d51d-049f-176905232806>
2014-08-31 08:23:11 +0000

    fix for Tkinter changes

--- branches/tk/src/skencil/Sketch/UI/skapp.py
+++ branches/tk/src/skencil/Sketch/UI/skapp.py
@@ -36,17 +36,17 @@ pixmaps = skpixmaps.PixmapTk
 # meta info defaults
 #
 meta_defaults = [
-    ('fullpathname', None),		# the full pathname if read from a file
-    ('filename', 'unnamed.sk'),		# filename without dir
-    ('directory', None),		# the directory
-    ('backup_created', 0),		# true if a backup has been created
+    ('fullpathname', None),# the full pathname if read from a file
+    ('filename', 'unnamed.sk'),# filename without dir
+    ('directory', None),# the directory
+    ('backup_created', 0),# true if a backup has been created
     ('format_name', plugins.NativeFormat),# the filetype (do we need this ?)
-    ('native_format', 1),		# whether the file was in native format
-    ('ps_directory', None),		# dir where PostScript file was created
-    ('ps_filename', ''),		# name of last postscript file
-    ('compressed', ''),			# was compressed (by gzip or bzip2)
-    ('compressed_file', ''),		# filename of compressed file
-    ('load_messages', ''),		# (warning) messages
+    ('native_format', 1),# whether the file was in native format
+    ('ps_directory', None),# dir where PostScript file was created
+    ('ps_filename', ''),# name of last postscript file
+    ('compressed', ''),# was compressed (by gzip or bzip2)
+    ('compressed_file', ''),# filename of compressed file
+    ('load_messages', ''),# (warning) messages
 ]
 
 for key, val in meta_defaults:
@@ -76,14 +76,14 @@ psfiletypes = (('PostScript', '.ps'),)
 
 imagefiletypes = ((_("All Files"), '*'),
                   (_("Encapsulated PostScript"), ('.eps', '.ps')),
-                  (_("JPEG"),	('.jpg', '.jpeg')),
-                  (_("GIF"),	'.gif'),
-                  (_("Portable Bitmap"),	'.pbm'),
-                  (_("Portable Graymap"),	'.pgm'),
-                  (_("Portable Pixmap"),	'.ppm'),
-                  (_("TIFF"),	('.tif', '.tiff')),
+                  (_("JPEG"), 	('.jpg', '.jpeg')),
+                  (_("GIF"), 	'.gif'),
+                  (_("Portable Bitmap"), 	'.pbm'),
+                  (_("Portable Graymap"), 	'.pgm'),
+                  (_("Portable Pixmap"), 	'.ppm'),
+                  (_("TIFF"), 	('.tif', '.tiff')),
                   (_("Windows / OS/2 Bitmap"), '.bmp'),
-                  (_("PCX"),	'.pcx'))
+                  (_("PCX"), 	'.pcx'))
 bitmapfiletypes = imagefiletypes[:1] + imagefiletypes[2:]
 
 
@@ -97,17 +97,18 @@ class TkApplication:
     tk_basename = ''
     tk_class_name = ''
 
-    def __init__(self, screen_name = None, geometry = None):
+    def __init__(self, screen_name=None, geometry=None):
         self.init_tk(screen_name, geometry)
 
-    def init_tk(self, screen_name = None, geometry = None):
-        self.root = Tk(screenName = screen_name,
-                       baseName = self.tk_basename,
-                       className = self.tk_class_name)
-        
+    def init_tk(self, screen_name=None, geometry=None):
+        self.root = Tk()
+#        self.root = Tk(screenName=screen_name,
+#                       baseName=self.tk_basename,
+#                       className=self.tk_class_name)
+
         gtkutils.set_ui_fonts(self.root, Sketch.ui_fonts)
         gtkutils.set_ui_colors(self.root, Sketch.ui_colors)
-        
+
         # Reset locale again to make sure we get properly translated
         # messages if desired by the user. For some reason it may
         # have been reset by Tcl/Tk.
@@ -178,13 +179,13 @@ class SketchApplication(TkApplication, Publisher):
     tk_basename = 'sketch'
     tk_class_name = 'Sketch'
 
-    def __init__(self, filename, screen_name = None, geometry = None,
-                 run_script = None):
+    def __init__(self, filename, screen_name=None, geometry=None,
+                 run_script=None):
         self.filename = filename
         self.geometry = geometry
         self.run_script = run_script
-        TkApplication.__init__(self, screen_name = screen_name,
-                               geometry = geometry)
+        TkApplication.__init__(self, screen_name=screen_name,
+                               geometry=geometry)
         self.build_window()
 
     def issue_clipboard(self):
@@ -197,8 +198,8 @@ class SketchApplication(TkApplication, Publisher):
         self.issue_clipboard()
 
     def AskUser(self, title, message):
-        return self.MessageBox(title = title, message = message,
-                               buttons = tkext.YesNo) == tkext.Yes
+        return self.MessageBox(title=title, message=message,
+                               buttons=tkext.YesNo) == tkext.Yes
 
     def Run(self):
         self.SetClipboard(None)
@@ -210,8 +211,8 @@ class SketchApplication(TkApplication, Publisher):
             height = self.root.winfo_reqheight()
             if width < 800 : width = 800
             if height < 600 : height = 600
-            posx = (self.root.winfo_screenwidth() - width)/2
-            posy = (self.root.winfo_screenheight() - height)/2
+            posx = (self.root.winfo_screenwidth() - width) / 2
+            posy = (self.root.winfo_screenheight() - height) / 2
             self.root.geometry('%dx%d%+d%+d' % (width, height, posx, posy))
         self.main_window.Run()
 
@@ -219,9 +220,9 @@ class SketchApplication(TkApplication, Publisher):
         pixmaps.clear_cache()
         self.root.destroy()
 
-    def init_tk(self, screen_name = None, geometry = None):
-        TkApplication.init_tk(self, screen_name = screen_name,
-                              geometry = geometry)
+    def init_tk(self, screen_name=None, geometry=None):
+        TkApplication.init_tk(self, screen_name=screen_name,
+                              geometry=geometry)
         root = self.root
         Sketch.init_modules_from_widget(root)
         root.iconbitmap(pixmaps.Icon)
